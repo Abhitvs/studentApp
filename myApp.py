a18d2b9398e1de969b4ac07931c87b8596d1d368
Abhitvs <abhi.nicklodean@gmail.com>
2025-11-25 11:11:47 +0530

    Add my application code

---
+++ myApp.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return {
+        "status": "SUCCESS",
+        "data":{
+            "regno":24122003,
+            "name":"Abhinav",
+            "email":"abhinav.tiwari@msds.christuniversity.in"
+
+        }
+    }
+
+if __name__ == "__main__":
+    app.run(debug=True)
