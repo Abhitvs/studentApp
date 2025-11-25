f3c3b6a22eab2a12f8e283b6350878ca81ab64c0
Abhitvs <abhi.nicklodean@gmail.com>
2025-11-25 11:37:11 +0530

    refactoring

---
+++ myApp.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return {
+        "status": "SUCCESS",
+        "data":{
+            "regno":24122003,
+            "name":"Abhinav",
+            "email":"abhinav.tiwari@msds.christuniversity.in"
+
+        }
+    }
+
+# TODO: create CRUD apis for this app
+
+if __name__ == "__main__":
+    app.run(debug=True)
